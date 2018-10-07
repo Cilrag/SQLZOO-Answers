d4e793dbddba59dbe1a2f0e4d6aee29e41bec257
Cilrag <33849632+Cilrag@users.noreply.github.com>
2018-10-07 00:53:02 -0400

    Create SUM and COUNT.sql

---
+++ SUM and COUNT.sql
@@ -0,0 +1,56 @@
+/*1*/
+/*Show the total population of the world.*/
+
+SELECT SUM(population)
+FROM world
+
+
+/*2*/
+/*List all the continents - just once each.*/
+
+SELECT DISTINCT continent FROM world
+
+
+/*3*/
+/*Give the total GDP of Africa*/
+
+SELECT SUM(GDP) FROM world WHERE continent='Africa'
+
+
+/*4*/
+/*How many Countries have an area of at least 1000000*/
+
+SELECT Count(name) FROM world 
+WHERE area>=1000000
+
+
+/*5*/
+/*What is the total population of ('Estonia', 'Latvia', 'Lithuania')*/
+
+SELECT SUM(population) FROM world 
+WHERE name IN ('Estonia', 'Latvia', 'Lithuania')
+
+
+/*6*/
+/*For each continent show the continent AND number of Countries.*/
+
+SELECT continent, Count(area) FROM world 
+GROUP BY continent
+
+
+/*7*/
+/*For each continent show the continent AND number of Countries with populations of at least 10 million.*/
+
+WHERE population>=10000000
+GROUP BY continent
+
+
+/*8*/
+/*List the continents that have a total population of at least 100 million.*/
+
+SELECT continent FROM world 
+GROUP BY continent
+HAVING SUM(population)>=100000000
+
+
+
